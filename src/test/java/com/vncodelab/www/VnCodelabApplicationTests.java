64e2f394314387406fc16d4b86543dd8b938ada4
xuanlam <phamxuanlam@gmail.com>
2021-02-10 14:02:32 +0700

    Init Project

---
+++ src/test/java/com/vncodelab/www/VnCodelabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vncodelab.www;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VnCodelabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
